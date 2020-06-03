ac73a4b8a1a56675f674973d23cd5e22c62aab4d
Takumi <shiotakumi@mineo.jp>
2020-06-04 00:36:40 +0900

    systan-app

---
+++ app.js
@@ -0,0 +1,98 @@
+const http = require('http');
+const fs = require('fs');
+const ejs = require('ejs');
+const url = require('url');
+const qs = require('querystring');
+
+const index_page = fs.readFileSync('./index.ejs','utf8');
+const style_css = fs.readFileSync('./style.css','utf8');
+const other_page = fs.readFileSync('./other.ejs','utf8');
+const chapter1Verbs_page = fs.readFileSync('./chapter1Verbs.ejs','utf8');
+
+var server = http.createServer(getFromClient);
+
+server.listen(process.env.PORT, process.env.IP);
+console.log('Server start!');
+
+function getFromClient(request,response){
+    var url_parts = url.parse(request.url,true);
+    switch (url_parts.pathname){
+        case '/':
+            response_index(request,response);
+            break;
+
+        case '/other':
+            response_other(request,response);
+            break;
+
+        case '/style.css':
+            response.writeHead(200,{'Content-Type':'text/css'});
+            response.write(style_css);
+            response.end();
+            break;
+
+        case '/chapter1Verbs':
+            var msg = "これはchapter1Verbsページです。"
+            var content = ejs.render(chapter1Verbs_page,{
+                content:msg
+            });
+            response.writeHead(200,{'Content-Type':'text/html'});
+            response.write(content);
+            response.end();
+            break;        
+        
+
+        default:
+            response.writeHead(200,{'Content-Type':'text/plain'});
+            response.end('no page');
+            break;
+    }
+}
+
+//indexのアクセス処理
+function response_index(request,response){
+    var msg = "これはIndexページです。"
+    var content = ejs.render(index_page,{
+        content:msg
+    });
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.write(content);
+    response.end();
+}
+
+//otherのアクセス処理
+function response_other(request,response){
+    var msg = "これはOtherページです。"
+    var content = ejs.render(other_page,{
+        content:msg
+    });
+    //POSTアクセス時の処理
+    if (request.method == 'POST'){
+        var body='';
+
+        //データ受信のイベント処理
+        request.on('data',(data)=>{
+            body+=data;
+        });
+        //データ受信終了のイベント処理
+        request.on('end',()=>{
+            var post_data=qs.parse(body);
+            msg+='あなたは'+post_data.msg+'と書きました';
+            var content = ejs.render(index_page,{
+                content:msg
+            });
+            response.writeHead(200,{'Content-Type':'text/html'});
+            response.write(content);
+            response.end();
+        });
+    //GETアクセス時の処理
+    }else{
+        var msg ="ページがありません"
+        var content = ejs.render(index_page,{
+            content:msg
+        });
+        response.writeHead(200,{'Content-Type':'text/html'});
+        response.write(content);
+        response.end();
+    }
+}
